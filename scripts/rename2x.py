3d0742adec923957bb750b5fa87f0900a47d8b14
1b0325h <melano@1b0325h.p001.jp>
2021-06-13 23:57:25 +0900

    add scripts

---
+++ scripts/rename2x.py
@@ -0,0 +1,14 @@
+import os
+import glob
+
+
+def rename2x():
+   """Add @2x to end of png file."""
+   for files in glob.glob("*.png"):
+      os.rename(files, files.replace(".png", "@2x") + ".png")
+
+
+if __name__ == "__main__":
+   # Place the png files you want to rename in same folder as this script file and run it.
+   os.chdir(os.path.dirname(os.path.abspath(__file__)))
+   rename2x()
---
+++ scripts/resize2x.py
@@ -0,0 +1,18 @@
+import os
+import glob
+
+from PIL import Image
+
+
+def resize2x():
+   """Resize of the @2x png file to half its original size and rename file."""
+   for files in glob.glob("*@2x.png"):
+      img = Image.open(files)
+      halve = img.resize((int(img.width * 0.5), int(img.height * 0.5)))
+      halve.save(files.replace("@2x.png", "") + ".png")
+
+
+if __name__ == "__main__":
+   # Place the png files you want to resize in same folder as this script file and run it.
+   os.chdir(os.path.dirname(os.path.abspath(__file__)))
+   resize2x()
