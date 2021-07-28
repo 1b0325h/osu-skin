40f7d72e722207259bbff5c7b2372257d405be03
1b0325h <melano@1b0325h.p001.jp>
2021-07-28 22:10:15 +0900

    v0
    Add a build script and change the directory accordingly.

---
+++ script/build.py
@@ -0,0 +1,41 @@
+import os
+import glob
+import shutil
+
+from PIL import Image
+
+
+def build():
+   def _name():
+      with open("../skin.ini", encoding="utf_8") as f:
+         for i in f.read().splitlines():
+            if i[:5] == "Name:":
+               return i.lstrip("Name: ")
+         else:
+            return "No_title"
+
+   def _ignore():
+      with open("../export/ignore.txt", encoding="utf_8") as f:
+         return f.read().splitlines()
+
+   def _copy(path, name):
+      for i in glob.glob(path):
+         shutil.copy(i, name)
+
+   name = _name()
+   ignore = _ignore()
+
+   os.mkdir(name)
+   shutil.copy("../skin.ini", name)
+
+   x = Image.new("RGBA", (1, 1), (0, 0, 0, 0))
+   for i in ignore:
+      x.save(f"{name}/{i}")
+
+   for path in ["../export/**/*.[pj][np]g", "../sound/*.[wm][ap][v3]"]:
+      _copy(path, name)
+
+
+if __name__ == "__main__":
+   os.chdir(os.path.dirname(os.path.abspath(__file__)))
+   build()
